886b61b910bc995b035aa968f0260224bdc839a4
Vytautas <v.astasauskas@gmail.com>
2022-02-24 13:38:41 +0200

    Added advanced functions for summary of 1500,1900 nm measurements

--- TubeTester/src/advanced.cpp
+++ TubeTester/src/advanced.cpp
@@ -4,6 +4,8 @@
 BEGIN_EVENT_TABLE(advanced, wxFrame)
 EVT_CLOSE(advanced::Close)
 EVT_BUTTON(controls::id::BFINDLAMBDA1300, advanced::OnFindLambda1300)
+EVT_BUTTON(controls::id::BFINDLAMBDA1500, advanced::OnFindLambda1500)
+EVT_BUTTON(controls::id::BFINDLAMBDA1900, advanced::OnFindLambda1900)
 END_EVENT_TABLE()
 
 advanced::advanced(wxPanel* parent, wxString title)
@@ -12,7 +14,12 @@ advanced::advanced(wxPanel* parent, wxString title)
 	MainWindow* myParent = (MainWindow*)m_parent->GetParent();
 	wxSizer* mainsizer = new wxBoxSizer(wxVERTICAL);
 	m_blambda1300 = new wxButton(this, controls::id::BFINDLAMBDA1300, "Lambda 1300 summary");
+	m_blambda1500 = new wxButton(this, controls::id::BFINDLAMBDA1500, "Lambda 1500 summary");
+	m_blambda1900 = new wxButton(this, controls::id::BFINDLAMBDA1900, "Lambda 1900 summary");
 	mainsizer->Add(m_blambda1300);
+	mainsizer->Add(m_blambda1500);
+	mainsizer->Add(m_blambda1900);
+	this->SetSizer(mainsizer);
 }
 
 void advanced::OnFindLambda1300(wxCommandEvent& event) {
@@ -59,14 +66,123 @@ void advanced::OnFindLambda1300(wxCommandEvent& event) {
 		else {
 			LOG(ERROR) << "Lambda=1300 file not found.";
 			wxMessageBox(wxT("Files do not exist in the lambda measurement directory"), wxT("Warning"), wxICON_WARNING);
+
 			break;
 		}
 		pathlambda1300 = wxFindNextFile();
 	}
 
-	wxMessageBox(wxT("Summary saved successfully"), wxT("Warning"), wxICON_WARNING);
+	wxMessageBox(wxT("Summary saved successfully to:\n") + pathlambda1300summaryCircle + "\n" + pathlambda1300summaryTotal, wxT("Warning"), wxICON_WARNING);
 	lambda1300SummaryCircleFile->Close();
 	lambda1300SummaryTotalFile->Close();
+}
+
+void advanced::OnFindLambda1500(wxCommandEvent& event) {
+	MainWindow* myParent = (MainWindow*)m_parent->GetParent();
+	pathlambda1500 = wxFindFirstFile(myParent->directory1500 + wxString::Format(wxT("%i/*.txt"), myParent->batchnumber));
+	pathlambda1500summaryCircle = myParent->directoryAnalysis + wxString::Format(wxT("%i_sumcir.txt"), myParent->batchnumber);
+	pathlambda1500summaryTotal = myParent->directoryAnalysis + wxString::Format(wxT("%i_sumtot.txt"), myParent->batchnumber);
+	lambda1500SummaryCircleFile = new wxTextFile(pathlambda1500summaryCircle);
+	lambda1500SummaryTotalFile = new wxTextFile(pathlambda1500summaryTotal);
+
+	if (wxFileExists(pathlambda1500summaryCircle)) {
+		wxRemoveFile(pathlambda1500summaryCircle);
+	}
+	lambda1500SummaryCircleFile->Create();
+	lambda1500SummaryCircleFile->Open();
+
+	if (wxFileExists(pathlambda1500summaryTotal)) {
+		wxRemoveFile(pathlambda1500summaryTotal);
+	}
+	lambda1500SummaryTotalFile->Create();
+	lambda1500SummaryTotalFile->Open();
+
+	while (!pathlambda1500.GetFullPath().empty())
+	{
+		lambdaFile = new wxTextFile(pathlambda1500.GetFullPath());
+		filenamelambda1500 = pathlambda1500.GetFullName();
+
+		if (wxFileExists(pathlambda1500.GetFullPath())) {
+			lambdaFile->Open();
+			arrstr = wxSplit(lambdaFile->GetLastLine(), '=');
+			totalval = wxAtoi(arrstr[1]);
+			arrstr = wxSplit(lambdaFile->GetPrevLine(), '=');
+			circleval = wxAtoi(arrstr[1]);
+			lambdaFile->Close();
+			arrstr = wxSplit(filenamelambda1500, '_');
+			fileID = arrstr[0] + "_" + arrstr[1];
+
+			lambda1500SummaryCircleFile->AddLine(fileID + " " + std::to_string(totalval));
+			lambda1500SummaryTotalFile->AddLine(fileID + " " + std::to_string(circleval));
+			lambda1500SummaryCircleFile->Write();
+			lambda1500SummaryTotalFile->Write();
+
+		}
+		else {
+			LOG(ERROR) << "Lambda=1500 file not found.";
+			wxMessageBox(wxT("Files do not exist in the lambda measurement directory"), wxT("Warning"), wxICON_WARNING);
+			break;
+		}
+		pathlambda1500 = wxFindNextFile();
+	}
+
+	wxMessageBox(wxT("Summary saved successfully to:\n") + pathlambda1500summaryCircle + "\n" + pathlambda1500summaryTotal, wxT("Warning"), wxICON_WARNING);
+	lambda1500SummaryCircleFile->Close();
+	lambda1500SummaryTotalFile->Close();
+}
+
+void advanced::OnFindLambda1900(wxCommandEvent& event) {
+	MainWindow* myParent = (MainWindow*)m_parent->GetParent();
+	pathlambda1900 = wxFindFirstFile(myParent->directory1900 + wxString::Format(wxT("%i/*.txt"), myParent->batchnumber));
+	pathlambda1900summaryCircle = myParent->directoryAnalysis + wxString::Format(wxT("%i_sumcir.txt"), myParent->batchnumber);
+	pathlambda1900summaryTotal = myParent->directoryAnalysis + wxString::Format(wxT("%i_sumtot.txt"), myParent->batchnumber);
+	lambda1900SummaryCircleFile = new wxTextFile(pathlambda1900summaryCircle);
+	lambda1900SummaryTotalFile = new wxTextFile(pathlambda1900summaryTotal);
+
+	if (wxFileExists(pathlambda1900summaryCircle)) {
+		wxRemoveFile(pathlambda1900summaryCircle);
+	}
+	lambda1900SummaryCircleFile->Create();
+	lambda1900SummaryCircleFile->Open();
+
+	if (wxFileExists(pathlambda1900summaryTotal)) {
+		wxRemoveFile(pathlambda1900summaryTotal);
+	}
+	lambda1900SummaryTotalFile->Create();
+	lambda1900SummaryTotalFile->Open();
+
+	while (!pathlambda1900.GetFullPath().empty())
+	{
+		lambdaFile = new wxTextFile(pathlambda1900.GetFullPath());
+		filenamelambda1900 = pathlambda1900.GetFullName();
+
+		if (wxFileExists(pathlambda1900.GetFullPath())) {
+			lambdaFile->Open();
+			arrstr = wxSplit(lambdaFile->GetLastLine(), '=');
+			totalval = wxAtoi(arrstr[1]);
+			arrstr = wxSplit(lambdaFile->GetPrevLine(), '=');
+			circleval = wxAtoi(arrstr[1]);
+			lambdaFile->Close();
+			arrstr = wxSplit(filenamelambda1900, '_');
+			fileID = arrstr[0] + "_" + arrstr[1];
+
+			lambda1900SummaryCircleFile->AddLine(fileID + " " + std::to_string(totalval));
+			lambda1900SummaryTotalFile->AddLine(fileID + " " + std::to_string(circleval));
+			lambda1900SummaryCircleFile->Write();
+			lambda1900SummaryTotalFile->Write();
+
+		}
+		else {
+			LOG(ERROR) << "Lambda=1900 file not found.";
+			wxMessageBox(wxT("Files do not exist in the lambda measurement directory"), wxT("Warning"), wxICON_WARNING);
+			break;
+		}
+		pathlambda1900 = wxFindNextFile();
+	}
+
+	wxMessageBox(wxT("Summary saved successfully to:\n") + pathlambda1900summaryCircle + "\n" + pathlambda1900summaryTotal, wxT("Warning"), wxICON_WARNING);
+	lambda1900SummaryCircleFile->Close();
+	lambda1900SummaryTotalFile->Close();
 }
 
 void advanced::Close(wxCloseEvent& event) {
--- TubeTester/src/advanced.h
+++ TubeTester/src/advanced.h
@@ -11,15 +11,32 @@ class advanced : public wxFrame
 public:
 	advanced(wxPanel* parent, wxString title); //!< Initializer for advanced functionality window, which contains buttons for executing various analysis procedures
 	void OnFindLambda1300(wxCommandEvent& event); //!< Command function for performing the 1300 nm wavelength analysis
+	void OnFindLambda1500(wxCommandEvent& event); //!< Command function for performing the 1500 nm wavelength analysis
+	void OnFindLambda1900(wxCommandEvent& event); //!< Command function for performing the 1900 nm wavelength analysis
 	void Close(wxCloseEvent& event); //!< Close event that is executed upon closing the window
 
 	wxButton* m_blambda1300; //!< Button for the 1300 nm wavelength analysis
+	wxButton* m_blambda1500; //!< Button for the 1500 nm wavelength analysis
+	wxButton* m_blambda1900; //!< Button for the 1900 nm wavelength analysis
+
 	wxFileName pathlambda1300; //!< Path holder for finding 1300 nm text files
-	wxString pathlambda1300summaryCircle; //!< Path holder for the text file into which the summary is written for the instensity in the defined circle
-	wxString pathlambda1300summaryTotal; //!< Path holder for the text file into which the summary is written for the instensity in the whole image
-	wxTextFile* lambda1300SummaryCircleFile; //!< Text file into which the summary is written for the instensity in the defined circle
-	wxTextFile* lambda1300SummaryTotalFile; //!< Text file into which the summary is written for the instensity in the whole image
+	wxFileName pathlambda1500; //!< Path holder for finding 1500 nm text files
+	wxFileName pathlambda1900; //!< Path holder for finding 1900 nm text files
+	wxString pathlambda1300summaryCircle; //!< Path holder for the text file into which the summary is written for the instensity in the defined circle of the 1300 nm image
+	wxString pathlambda1500summaryCircle; //!< Path holder for the text file into which the summary is written for the instensity in the defined circle of the 1500 nm image
+	wxString pathlambda1900summaryCircle; //!< Path holder for the text file into which the summary is written for the instensity in the defined circle of the 1900 nm image
+	wxString pathlambda1300summaryTotal; //!< Path holder for the text file into which the summary is written for the instensity in the whole 1300 nm image image
+	wxString pathlambda1500summaryTotal; //!< Path holder for the text file into which the summary is written for the instensity in the whole 1500 nm image image
+	wxString pathlambda1900summaryTotal; //!< Path holder for the text file into which the summary is written for the instensity in the whole 1900 nm image image
+	wxTextFile* lambda1300SummaryCircleFile; //!< Text file into which the summary is written for the intensity in the defined circle of the 1300 nm image
+	wxTextFile* lambda1500SummaryCircleFile; //!< Text file into which the summary is written for the intensity in the defined circle of the 1500 nm image
+	wxTextFile* lambda1900SummaryCircleFile; //!< Text file into which the summary is written for the intensity in the defined circle of the 1900 nm image
+	wxTextFile* lambda1300SummaryTotalFile; //!< Text file into which the summary is written for the instensity in the whole 1300 nm image
+	wxTextFile* lambda1500SummaryTotalFile; //!< Text file into which the summary is written for the instensity in the whole 1500 nm image
+	wxTextFile* lambda1900SummaryTotalFile; //!< Text file into which the summary is written for the instensity in the whole 1900 nm image
 	wxString filenamelambda1300; //!< Filename of the file that is being read (1300 nm)
+	wxString filenamelambda1500; //!< Filename of the file that is being read (1500 nm)
+	wxString filenamelambda1900; //!< Filename of the file that is being read (1900 nm)
 
 	wxTextFile* lambdaFile; //!< Text file which is read for the chosen wavelength
 	wxString fileID; //!< ID of the tube of which the file is being read, e.g. 2001_00079
